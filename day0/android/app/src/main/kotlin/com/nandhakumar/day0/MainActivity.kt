88994cfd292117f7643b61a5609e8f70ba092764
nandhakumarbm <nandhakumarbm7@gmail.com>
2025-12-08 11:39:10 +0530

    chore: add day0..day7 Flutter project skeletons

---
+++ day0/android/app/src/main/kotlin/com/nandhakumar/day0/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.nandhakumar.day0
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ day4/android/app/src/main/kotlin/com/nandhakumar/day4/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.nandhakumar.day4
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
